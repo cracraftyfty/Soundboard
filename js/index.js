63f330776adeea9d0ecd13dd17c7c2eb77d77055
cracraftyfty <saadzaheer202@gmail.com>
2023-09-08 21:23:31 +0530

    New Button And bug fix
    [ADDED]
    - Random button for playing random sounds
    
    [FIXES]
    - Fixed the issue of sound overlapping when multiple buttons are pressed

--- js/index.js
+++ js/index.js
@@ -1,11 +1,23 @@
+let currentAudio = null;
+
 document.querySelectorAll(".drum").forEach((u) =>
   u.addEventListener("click", function () {
+    if (currentAudio) {
+      currentAudio.pause();
+      currentAudio.currentTime = 0;
+    }
+
     makeSound(u.innerHTML);
     doAnimation(u.innerHTML);
   })
 );
 
 document.addEventListener("keydown", function (event) {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+  }
+
   makeSound(event.key);
   doAnimation(event.key);
 });
@@ -17,36 +29,51 @@ function makeSound(key) {
       audio = new Audio("./sounds/crash.mp3");
       audio.volume = 1;
       audio.play();
+      currentAudio = audio;
       break;
     case "2":
       audio = new Audio("./sounds/kick-bass.mp3");
       audio.volume = 0.4;
       audio.play();
+      currentAudio = audio;
       break;
     case "3":
       audio = new Audio("./sounds/snare.mp3");
       audio.volume = 0.6;
       audio.play();
+      currentAudio = audio;
       break;
     case "4":
       audio = new Audio("./sounds/tom-1.mp3");
       audio.volume = 0.5;
       audio.play();
+      currentAudio = audio;
       break;
     case "5":
       audio = new Audio("./sounds/tom-2.mp3");
       audio.volume = 0.4;
       audio.play();
+      currentAudio = audio;
       break;
     case "6":
       audio = new Audio("./sounds/tom-3.mp3");
       audio.volume = 1;
       audio.play();
+      currentAudio = audio;
       break;
     case "7":
       audio = new Audio("./sounds/tom-4.mp3");
       audio.volume = 0.6;
       audio.play();
+      currentAudio = audio;
+      break;
+    case "r":
+      let soundArr = ["bing-chilllllling.mp3", "crash.mp3", "kick-bass.mp3", "snare.mp3", "tom-1.mp3", "tom-2.mp3", "tom-3.mp3", "tom-4.mp3"];
+      let randomSound = soundArr[Math.floor(Math.random() * soundArr.length)];
+      audio = new Audio(`./sounds/${randomSound}`);
+      audio.volume = 0.5;
+      audio.play();
+      currentAudio = audio;
       break;
     default:
   }
@@ -61,9 +88,10 @@ function doAnimation(key) {
     5: "five",
     6: "six",
     7: "seven",
+    r: "r",
   };
   var activeButton = document.querySelector(`.${keyDir[key]}`);
-
+  console.log(activeButton);
   activeButton.classList.add(`pressed`);
   setTimeout(() => {
     activeButton.classList.remove("pressed");
